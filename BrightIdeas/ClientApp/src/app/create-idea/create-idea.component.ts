3471d52ba8b97934320cbafe7cee5409d1cf63d7
Shaun Richardson <srichardson6@myseneca.ca>
2020-03-30 22:07:45 -0400

    Updated main Nav bar, fixed spacing on pages

--- BrightIdeas/ClientApp/src/app/create-idea/create-idea.component.ts
+++ BrightIdeas/ClientApp/src/app/create-idea/create-idea.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-create-idea',
   templateUrl: './create-idea.component.html',
+  styleUrls: ['./create-idea.component.css']
 })
 export class CreateIdeaComponent {
 }
--- BrightIdeas/ClientApp/src/app/project/project.component.ts
+++ BrightIdeas/ClientApp/src/app/project/project.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-project',
   templateUrl: './project.component.html',
+  styleUrls: ['./project.component.css']
 })
 export class ProjectComponent {
 }
