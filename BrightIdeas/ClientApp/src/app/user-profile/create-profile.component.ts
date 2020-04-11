557f71197b9546b9c6a21c60a7f594d1eaf4d53e
Shaun Richardson <srichardson6@myseneca.ca>
2020-04-11 17:09:31 -0400

    Create profile: UI

--- BrightIdeas/ClientApp/src/app/user-profile/create-profile.component.ts
+++ BrightIdeas/ClientApp/src/app/user-profile/create-profile.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-create-profile',
   templateUrl: './create-profile.component.html',
+  styleUrls: ['./create-profile.component.css']
 })
 export class CreateProfileComponent {
 }
